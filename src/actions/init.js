55beefa95c5361902ec1b25f9c8f756a6084dc28
GuttermanA <alexanderf.gutterman@gmail.com>
2018-07-02 17:26:31 -0400

    commiting before new branch

--- src/actions/init.js
+++ src/actions/init.js
@@ -1,7 +1,11 @@
 import { API_ROOT } from '../globalVars'
 
 export const fetchMetaData = () => {
+
   return (dispatch) => {
+    dispatch({
+      type: 'LOADING'
+    })
     return (
       fetch(`${API_ROOT}/metadata_load`)
         .then(res => res.json())
